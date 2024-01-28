957a065cd8d303761b5efa104849d73a4b017295
Tanuj Arora <Tanujarora2703@gmail.com>
2024-01-29 00:46:06 +0530

    User atomicity is improved

--- backend/routes/userRoute.ts
+++ backend/routes/userRoute.ts
@@ -12,6 +12,7 @@ import authenticateUser from "../middleware/Auth";
 import { userUpdateValidator } from "../validation/userUpdateValidation";
 import IUser from "../interfaces/IUser";
 import Account from "../models/Account";
+import mongoose from "mongoose";
 userRouter.get(
   "/signin",
   checkUserExist,
@@ -41,7 +42,10 @@ userRouter.post(
   validateUserSignIn,
   checkExistingUser,
   async (req: Request, res: Response) => {
+    const session = mongoose.startSession();
     try {
+      (await session).startTransaction();
+      logger.info("transaction started");
       const { username, password, firstname, lastname } = req.body;
       const { success } = userSchemaValidator.safeParse(req.body);
       if (!success) {
@@ -49,6 +53,7 @@ userRouter.post(
           message: "Incorrect Inputs",
         });
       }
+      logger.info("input validation completed");
       //create a new User
       const newUser = await User.create({
         username: username,
@@ -57,26 +62,38 @@ userRouter.post(
         lastname: lastname,
       });
       const token = signJWT({ userId: newUser._id });
+      logger.info("token signing completed");
       //hash the password
       newUser.password = newUser.hashPassword(password);
       const userAccount = await Account.create({
-        userId: newUser._id,
+        id: newUser._id,
         balanced: Math.random() * 1000,
       });
-      //giving the userAccount some initial balance for mocking 
+      logger.info("user Account created");
+      //giving the userAccount some initial balance for mocking
       //using hashsync so it will return a string
 
       //Save the user in DB
-
+      newUser.password = newUser.hashPassword(password);
+      logger.info("password hashed");
+      await newUser.save();
       //sign a jwt token and send it to the user.
       logger.info(`User with ${newUser.username} created`);
+
+      (await session).commitTransaction();
+      logger.info("Transaction committed");
       res.status(201).json({
         message: "user created successfully",
         token: token,
       });
     } catch (err) {
+      (await session).abortTransaction();
+      logger.info("Transaction aborted");
       logger.info(err);
       return res.status(500).json({ error: "Server Error" });
+    } finally {
+      logger.info("Transaction completed");
+      (await session).endSession();
     }
   },
 
