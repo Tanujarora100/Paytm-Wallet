59a1bc2ea50b68374f836bd1b1a624c32bbf9135
Tanuj Arora <Tanujarora2703@gmail.com>
2024-01-28 08:27:28 +0530

    First Commit

---
+++ backend/models/User.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+import IUser from "../interfaces/IUser";
+
+const userSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model<IUser>("User", userSchema);
