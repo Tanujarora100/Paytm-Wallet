4c4f4d410480375477f7603531276c55f543d5ab
Tanuj Arora <Tanujarora2703@gmail.com>
2024-01-28 10:43:26 +0530

    UserSignInValidation and JWT Logic Added

--- backend/routes/userRoute.ts
+++ backend/routes/userRoute.ts
@@ -6,38 +6,73 @@ import jwt_secret from "../config/jwt_secret";
 import { JWT_EXPIRY_SECONDS } from "../config/jwt_secret";
 import jwt, { Secret } from "jsonwebtoken";
 import logger from "../config/logger";
+import checkUserExist from "../middleware/checkUserExist";
 const userRouter = Router();
+import { Request, Response } from "express";
+import { validateUserSignIn } from "../validation/userSignInValidation";
 
-userRouter.post("/signup", checkExistingUser, async (req, res) => {
-  try {
-    const { username, password, firstname, lastname } = req.body;
-    const { success } = userSchemaValidator.safeParse(req.body);
-    if (!success) {
-      return res.send(411).json({
-        message: "Incorrect Inputs",
+userRouter.get(
+  "/signin",
+  checkUserExist,
+  async (req: Request, res: Response) => {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+
+      if (user && user.password !== password) {
+        return res.send(400).json({ error: "Incorrect Password" });
+      }
+      const userId = user?._id;
+      logger.info(`User with ${username} send a request`);
+      const token = jwt.sign({ userId }, jwt_secret as unknown as Secret, {
+        expiresIn: JWT_EXPIRY_SECONDS,
       });
+      res.status(200).json({
+        message: "user signed in successfully",
+        token: token,
+      });
+    } catch (err) {
+      logger.error(err);
+      return res.status(500).json({ error: "Server Error" });
     }
-    const newUser = await User.create({
-      username: username,
-      password: password,
-      firstname: firstname,
-      lastname: lastname,
-    });
+  },
+);
 
-    const userId = newUser._id;
+userRouter.post(
+  "/signup",
+  validateUserSignIn,
+  checkExistingUser,
+  async (req: Request, res: Response) => {
+    try {
+      const { username, password, firstname, lastname } = req.body;
+      const { success } = userSchemaValidator.safeParse(req.body);
+      if (!success) {
+        return res.send(411).json({
+          message: "Incorrect Inputs",
+        });
+      }
+      const newUser = await User.create({
+        username: username,
+        password: password,
+        firstname: firstname,
+        lastname: lastname,
+      });
 
-    const token = jwt.sign({ userId }, jwt_secret as unknown as Secret, {
-      expiresIn: JWT_EXPIRY_SECONDS,
-    });
+      const userId = newUser._id;
+      logger.info(`User with ${username} created`);
+      const token = jwt.sign({ userId }, jwt_secret as unknown as Secret, {
+        expiresIn: JWT_EXPIRY_SECONDS,
+      });
 
-    res.status(201).json({
-      message: "user created successfully",
-      token: token,
-    });
-  } catch (err) {
-    logger.info(err);
-    return res.status(500).json({ error: "Server Error" });
-  }
-});
+      res.status(201).json({
+        message: "user created successfully",
+        token: token,
+      });
+    } catch (err) {
+      logger.info(err);
+      return res.status(500).json({ error: "Server Error" });
+    }
+  },
+);
 
 export default userRouter;
--- backend/validation/userSignInValidation.ts
+++ backend/validation/userSignInValidation.ts
@@ -1,5 +1,27 @@
+import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
-export const userSignInValidator = z.object({
-  username: z.string().email(),
+
+const userSignInSchema = z.object({
+  username: z.string().min(3),
   password: z.string().min(6),
 });
+
+export function validateUserSignIn(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const { success } = userSignInSchema.safeParse(req.body);
+    if (!success) {
+      return res.status(400).json({
+        message: "Incorrect Sign-In Inputs",
+      });
+    }
+    next();
+  } catch (error) {
+    res.status(400).json({
+      message: "Incorrect Sign-In Inputs",
+    });
+  }
+}
